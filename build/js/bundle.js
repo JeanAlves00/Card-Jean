820c72e28a6775752d4ed885d5cd4db3e9bf897d
Jean <jepaalgo00@gmail.com>
2024-06-04 10:51:04 -0400

    0.1

---
+++ build/js/bundle.js
@@ -0,0 +1,10 @@
+document.getElementById('expandButton').addEventListener('click', function() {
+  var extraContent = document.getElementById('extraContent');
+  if (extraContent.classList.contains('hidden')) {
+      extraContent.classList.remove('hidden');
+      this.textContent = 'Clique para Recolher';
+  } else {
+      extraContent.classList.add('hidden');
+      this.textContent = 'Clique para Expandir';
+  }
+});
